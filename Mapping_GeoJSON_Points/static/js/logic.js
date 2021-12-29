f85ac755bf893a54a035caf5b91549827469c602
Megan Fink <finkers13.com>
2021-12-29 15:11:04 -0600

    fix

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,40 +1,43 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-// use onEachFeature
-  onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2>" + "<h3> Airport name: " + feature.properties.name + "</h3>")
-  }
-}).addTo(map);
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+// // use onEachFeature
+//   onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2>" + "<h3> Airport name: " + feature.properties.name + "</h3>")
+//   }
+// }).addTo(map);
 
 // We turn each feature into a marker on the map.
 //     pointToLayer: function(feature, latlng) {
