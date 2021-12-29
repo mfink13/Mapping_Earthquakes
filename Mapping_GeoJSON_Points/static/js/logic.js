9a175dc2e5ca68c6d68b04dd53b8e5d0096b7952
Megan Fink <finkers13.com>
2021-12-29 15:46:28 -0600

    skill drill complete

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,6 +4,35 @@ console.log("working");
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
+    // We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+});
+
+// Then we add out 'streets' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/mfink13/Mapping_Earthquakes/main/majorAirports.json";
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(features, layer){
+          console.log(layer);
+          layer.bindPopup("<h2>Airport code: " + features.properties.faa + "</h2>" + "<h3> Airport name: " + features.properties.name + "</h3>")
+      }
+  }).addTo(map);
+});
+
+
+
 // // Create the map object with center at the San Francisco airport.
 // let map = L.map('mapid').setView([37.5, -122.5], 10);
 
@@ -48,13 +77,13 @@ let map = L.map('mapid').setView([30, 30], 2);
 
 //   }).addTo(map);
 
-    // We create the tile layer that will be the background of our map.
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        accessToken: API_KEY
-});
+//     // use onEachFeature
+//       onEachFeature: function(feature, layer) {
+//           console.log(layer);
+//           layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2>" + "<h3> Airport name: " + feature.properties.name + "</h3>")
+    //   }
+    // }).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
@@ -65,4 +94,4 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-pr
 // });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
